07fededf91b392562abc005a82b2d86d3cc8379f
Jonathan A <jony1299@gmail.com>
2023-06-30 11:14:08 -0400

    update eval script

--- eval.py
+++ eval.py
@@ -11,10 +11,15 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)  # Batch Normalization after each conv layer
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
         self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
         self.conv5 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(1024)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.dropout = nn.Dropout(p=0.5)
         self.fc1 = nn.Linear(1024 * 2 * 2, 1024)
@@ -23,11 +28,11 @@ class Net(nn.Module):
         self.fc4 = nn.Linear(256, 2)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
-        x = self.pool(F.relu(self.conv5(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        x = self.pool(F.relu(self.bn5(self.conv5(x))))
         x = x.view(-1, 1024 * 2 * 2)
         x = self.dropout(x)
         x = F.relu(self.fc1(x))
