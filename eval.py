00c1e860d73d1e8fe47112aa682bd5601433915d
Jonathan A <jony1299@gmail.com>
2023-06-30 11:12:41 -0400

    lr scheduler + data transformation

--- eval.py
+++ eval.py
@@ -2,6 +2,7 @@ import torch
 import torchvision.transforms as transforms
 import torch.nn as nn
 from torchvision.datasets import ImageFolder
+import torch.nn.functional as F
 from PIL import Image
 import os
 
@@ -9,28 +10,30 @@ import os
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.dropout = nn.Dropout(p=0.5)
-        self.fc1 = nn.Linear(256 * 2 * 2, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 2)
+        self.fc1 = nn.Linear(1024 * 2 * 2, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 2)
 
     def forward(self, x):
-        x = self.pool(nn.functional.relu(self.conv1(x)))
-        x = self.pool(nn.functional.relu(self.conv2(x)))
-        x = self.pool(nn.functional.relu(self.conv3(x)))
-        x = self.pool(nn.functional.relu(self.conv4(x)))
-        x = self.pool(nn.functional.relu(self.conv5(x)))
-        x = x.view(-1, 256 * 2 * 2)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool(F.relu(self.conv5(x)))
+        x = x.view(-1, 1024 * 2 * 2)
         x = self.dropout(x)
-        x = nn.functional.relu(self.fc1(x))
-        x = nn.functional.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
 model = Net()
--- train.py
+++ train.py
@@ -3,18 +3,25 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+from torch.optim import lr_scheduler
 import torch.nn.functional as F
 from tqdm import tqdm
+import os
 
 # define neural net
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)  # Batch Normalization after each conv layer
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
         self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
         self.conv5 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(1024)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.dropout = nn.Dropout(p=0.5)
         self.fc1 = nn.Linear(1024 * 2 * 2, 1024)
@@ -23,11 +30,11 @@ class Net(nn.Module):
         self.fc4 = nn.Linear(256, 2)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
-        x = self.pool(F.relu(self.conv5(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        x = self.pool(F.relu(self.bn5(self.conv5(x))))
         x = x.view(-1, 1024 * 2 * 2)
         x = self.dropout(x)
         x = F.relu(self.fc1(x))
@@ -38,22 +45,34 @@ class Net(nn.Module):
 
 model = Net()
 
-# load and label the images
-data_transform = transforms.Compose([
-    transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-])
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(64),  # For data augmentation
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(64),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ]),
+}
 
 dataset_path = 'sets'
-dataset = datasets.ImageFolder(dataset_path, transform=data_transform)
+dataset = {x: datasets.ImageFolder(os.path.join(dataset_path, x), data_transforms[x]) for x in ['train', 'val']}
 
 # create data loader
-train_loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+dataloaders = {x: torch.utils.data.DataLoader(dataset[x], batch_size=32, shuffle=True) for x in ['train', 'val']}
 
 # define loss
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)  # Use SGD with momentum
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
 # train model
 num_epochs = 20
@@ -62,7 +81,7 @@ model.to(device)
 
 for epoch in range(num_epochs):
     running_loss = 0.0
-    pbar = tqdm(train_loader, ncols=80)
+    pbar = tqdm(dataloaders['train'], ncols=80)
     for inputs, labels in pbar:
         inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
@@ -71,9 +90,12 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        pbar.set_postfix({'Loss': running_loss / (len(train_loader))})
+        pbar.set_postfix({'Loss': running_loss / (len(dataloaders['train']))})
+
+    exp_lr_scheduler.step()  # Step in learning rate scheduler
+
     pbar.close()
-    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / (len(train_loader))}")
+    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / (len(dataloaders['train']))}")
 
 # save model
 torch.save(model.state_dict(), 'my_model.pth')
